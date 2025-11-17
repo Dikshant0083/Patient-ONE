01ef2c2e8ed0623b0734c656a2571826a23247b0
dikshant0083 <khileridikshant@gmail.com>
2025-11-17 12:39:49 +0530

    google auth and testing updated

--- config/passport.js
+++ config/passport.js
@@ -32,7 +32,7 @@ const configurePassport = () => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'https://localhost:3000/auth/google/callback',
     passReqToCallback: true,
   }, async (req, accessToken, refreshToken, profile, done) => {
     try {
